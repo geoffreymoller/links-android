1758324b1da00e2fedf7ee8d90206cdfacfb5161
Geoff Moller <geoff.moller@gmail.com>
2013-12-02 20:23:42 -0800

    search query quick fix for resume

--- Links/src/main/java/com/geoffreymoller/links/MainActivity.java
+++ Links/src/main/java/com/geoffreymoller/links/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 public class MainActivity extends SingleFragmentActivity {
 
     //TODO - webview
-    //TODO - fix onResume Activity crash
+    //TODO - stash and retrieve search term on suspend
 
     private static final String TAG = "MainActivity";
 
@@ -20,6 +20,7 @@ public class MainActivity extends SingleFragmentActivity {
     protected void onNewIntent(Intent intent) {
         setIntent(intent);
         String query = intent.getStringExtra(SearchManager.QUERY);
+        query = query != null ? query : "";
         //TODO - set non-generic fragment id
         LinkListFragment f = (LinkListFragment) getFragmentManager().findFragmentById(R.id.fragmentContainer);
         f.search(query);
